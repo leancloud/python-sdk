a48e9d468f994f4a92a5fba5b902a60685230c8b
asaka <aisk1988@gmail.com>
2014-11-24 16:42:25 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -5,35 +5,20 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='leancloud-sdk',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
     version='1.0.0',
-
     description='Leancloud Python SDK',
 
-    # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://leancloud.cn/',
 
-    # Author details
     author='asaka',
     author_email='lan@leancloud.rocks',
 
-    # Choose your license
-    license='MIT',
+    license='BSD-2-Clause',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
 
         'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
@@ -46,10 +31,6 @@ setup(
 
     test_suite='nose.collector',
 
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=[
         'requests',
     ],
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-from pprint import pprint
-
 from nose.tools import eq_
 from nose.tools import with_setup
 
