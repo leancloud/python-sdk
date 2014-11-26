527a6f95330f405e422e4ba5f89eef1ea4e4e3dd
asaka <aisk1988@gmail.com>
2014-11-26 18:07:25 +0800

    update AVObject

--- leancloud/object_.py
+++ leancloud/object_.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 
+import copy
+
 from leancloud import rest
 from leancloud.fields import AnyField
+from leancloud import operation
 
 
 __author__ = 'asaka <lan@leancloud.rocks>'
@@ -84,9 +87,13 @@ class Object(object):
         return getattr(self, key)
 
 
-'''
 class AVObject(object):
-    def __init__(self, attributes):
+    def __init__(self, attributes=None):
+        if not attributes:
+            attributes = {}
+
+        # TODO: get defaults
+
         self.id = None
 
         self._server_data = {}
@@ -100,11 +107,93 @@ class AVObject(object):
         self._silent = {}
         self._pending = {}
 
+        self._fetch_when_save = False
+
+    def fetch_when_save(self, enable):
+        self._fetch_when_save = enable
+
+    def dump(self):
+        obj = self._dump()
+        obj.pop('__type')
+        obj.pop('className')
+        return obj
+
+    def _dump(self, seen_objects=False):
+        obj = copy.deepcopy(self.attributes)
+        for k, v in obj:
+            obj[k] = encode(v, seen_objects)
+
+        for k, v in self._operations:
+            obj[k] = v
+
+        if hasattr(self, 'id'):
+            obj['objectId'] = self.id
+
+        if hasattr(self, 'createdAt'):
+            # TODO: parse date
+            obj['createdAt'] = self.createdAt
+
+        if hasattr(self, 'updatedAt'):
+            # TODO: parse date
+            obj['updatedAt'] = self.updatedAt
+
+        obj['__type'] = 'Object'
+        obj['className'] = self.__class__.__name__
+        return obj
+
+    def _refresh_cache(self):
+        # TODO
+        if hasattr(self, '_refreshing_cache'):
+            return
+        self._refreshing_cache = True
+        for k, v in self.attributes:
+            if isinstance(v, AVObject):
+                v._refresh_cache()
+            elif isinstance(v, dict):
+                if self._refresh_cache_for_key(k):
+                    self.set(k, operation.Set(v), silent=True)
+        del self._refreshing_cache
+
+    def dirty(self, attr=None):
+        self._refresh_cache()
+        current_changes = self._op_set_queue[-1]
+        if attr is not None:
+            #TODO
+            pass
+
+        if not hasattr(self, 'id'):
+            return True
+
+        # TODO: handle current changes
+
+        return False
+
+    def _to_pointer(self):
+        return {
+            '__type': 'Pointer',
+            'className': self.__class__.__name__,
+            'objectId': self.id,
+        }
+
     def get(self, attr):
         return self.attributes[attr]
 
+    def relation(self, attr):
+        # TODO
+        pass
+
     def has(self, attr):
         return attr in self.attributes
+
+    def _merge_magic_field(self, attrs):
+        pass
+
+    def _start_save(self):
+        self._op_set_queue.append({})
+
+    def _cancel_save(self):
+        failed_changes = self._op_set_queue.pop(0)
+        next_changes = self._op_set_queue[0]
 
     def set(self, key, value):
         pass
@@ -112,8 +201,8 @@ class AVObject(object):
     def unset(self, attr):
         pass
 
-    def increment(self, attr, amount):
-        pass
+    def increment(self, attr, amount=1):
+        return self.set(attr, operation.Increment(amount))
 
     def add(self, attr, item):
         pass
@@ -153,4 +242,3 @@ class AVObject(object):
 
     def change(self):
         pass
-'''
---
+++ leancloud/operation.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+__author__ = 'asaka'
+
+
+class Operation(object):
+    pass
+
+
+class Set(Operation):
+    pass
+
+
+class Unset(Operation):
+    pass
+
+
+class Increment(Operation):
+    pass
+
+
+class Add(Operation):
+    pass
+
+
+class AddUnique(Operation):
+    pass
+
+
+class Remove(Operation):
+    pass
+
+
+class Relation(Operation):
+    pass
