66f9303a139db0d51fd9a887964702cc0f63b6c3
asaka <aisk1988@gmail.com>
2015-03-18 15:46:21 +0800

    change API doc to English

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ release = '1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+language = 'en_US'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
