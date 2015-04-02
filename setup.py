4b6b3df2af5a58d69b122c21b05815950e2b0a89
asaka <aisk1988@gmail.com>
2015-04-02 16:43:34 +0800

    specify requests version > 2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='leancloud-sdk',
-    version='1.0.1',
+    version='1.0.2',
     description='LeanCloud Python SDK',
 
     url='https://leancloud.cn/',
@@ -33,7 +33,7 @@ setup(
 
     install_requires=[
         'qiniu',
-        'requests',
+        'requests>2.4.0',
         'iso8601',
     ],
 
