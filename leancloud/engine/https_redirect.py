5b4e92920af11e58314c450e340b26fa24c62fca
asaka <aisk1988@gmail.com>
2015-05-21 16:55:19 +0800

    https redirect only works on production env

--- leancloud/engine/https_redirect.py
+++ leancloud/engine/https_redirect.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 
+import os
+
 from werkzeug.wrappers import Request
 from werkzeug.utils import redirect
 
 __author__ = 'asaka <lan@leancloud.rocks>'
+
+
+is_prod = int(os.environ.get('LC_APP_PROD', '0'))
 
 
 class HttpsRedirectMiddleware(object):
@@ -12,7 +17,7 @@ class HttpsRedirectMiddleware(object):
 
     def __call__(self, environ, start_response):
         request = Request(environ)
-        if request.headers.get('X-Forwarded-Proto') == 'http':
+        if is_prod and request.headers.get('X-Forwarded-Proto') != 'https':
             url = 'https://{0}{1}'.format(request.host, request.path)
             if request.query_string:
                 url += '?{}'.format(request.query_string)
