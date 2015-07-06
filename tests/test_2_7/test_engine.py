a2b1f5acb0b0b51c0e9563102b5fc46cdff258a4
DionysusG <yuan.gan@fandm.edu>
2015-07-06 19:07:08 +0800

    travis updated

---
+++ tests/test_2_7/test_engine.py
@@ -0,0 +1,178 @@
+# coding: utf-8
+
+import requests
+
+from wsgi_intercept import requests_intercept, add_wsgi_intercept
+
+
+import leancloud
+from leancloud import Engine
+from leancloud import cloudfunc
+from leancloud.engine import authorization
+
+
+__author__ = 'asaka <lan@leancloud.rocks>'
+
+env = None
+
+TEST_APP_ID = 'mdx1l0uh1p08tdpsk8ffn4uxjh2bbhl86rebrk3muph08qx7'
+TEST_APP_KEY = 'n35a5fdhawz56y24pjn3u9d5zp9r1nhpebrxyyu359cq0ddo'
+TEST_MASTER_KEY = 'h2ln3ffyfzysxmkl4p3ja7ih0y6sq5knsa2j0qnm1blk2rn2'
+
+NORMAL_HEADERS = {
+    'x-avoscloud-application-id': TEST_APP_ID,
+    'x-avoscloud-application-key': TEST_APP_KEY,
+}
+
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Hello LeanCloud']
+
+
+engine = Engine(app)
+
+
+def make_app():
+    return engine
+
+
+host, port = 'localhost', 80
+url = 'http://{0}:{1}/'.format(host, port)
+
+
+def setup():
+    leancloud.init(TEST_APP_ID, TEST_APP_KEY)
+    authorization._ENABLE_TEST = True
+    authorization.APP_ID = TEST_APP_ID
+    authorization.APP_KEY = TEST_APP_KEY
+    authorization.MASTER_KEY = TEST_MASTER_KEY
+
+    requests_intercept.install()
+    add_wsgi_intercept(host, port, make_app)
+
+    @engine.define
+    def hello(**params):
+        return 'hello'
+
+
+def teardown():
+    requests_intercept.uninstall()
+
+
+def test_origin_response():
+    resp = requests.get(url)
+    assert resp.ok
+    assert resp.content == 'Hello LeanCloud'
+
+
+def test_compatibility():
+    requests.get(url + '/1/functions/hello')
+    assert '_app_params' in authorization.current_environ
+
+    requests.get(url + '/1.1/functions/hello')
+    assert '_app_params' in authorization.current_environ
+
+
+def test_app_params_1():
+    requests.get(url + '/__engine/1/functions/hello')
+    assert '_app_params' in authorization.current_environ
+
+
+def test_app_params_2():
+    resp = requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-application-id': 'foo',
+        'x-avoscloud-application-key': 'bar',
+        'x-avoscloud-session-token': 'baz',
+    })
+    env = authorization.current_environ
+    assert env['_app_params']['id'] == 'foo'
+    assert env['_app_params']['key'] == 'bar'
+    assert env['_app_params']['session_token'] == 'baz'
+
+
+def test_app_params_3():
+    requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-request-sign': '28ad0513f8788d58bb0f7caa0af23400,1389085779854'
+    })
+    env = authorization.current_environ
+    assert env['_app_params']['key'] == 'n35a5fdhawz56y24pjn3u9d5zp9r1nhpebrxyyu359cq0ddo'
+
+
+def test_app_params_4():
+    requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-request-sign': 'c884fe684c17c972eb4e33bc8b29cb5b,1389085779854,master'
+    })
+    env = authorization.current_environ
+    assert env['_app_params']['key'] == 'h2ln3ffyfzysxmkl4p3ja7ih0y6sq5knsa2j0qnm1blk2rn2'
+
+
+def test_authorization_1():
+    response = requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-application-id': TEST_APP_ID,
+        'x-avoscloud-application-key': TEST_APP_KEY,
+    })
+    assert response.ok
+    assert response.json() == {u'result': u'hello'}
+
+
+def test_authorization_2():
+    response = requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-application-id': TEST_APP_ID,
+        'x-avoscloud-application-key': TEST_MASTER_KEY,
+    })
+    assert response.ok
+    assert response.json() == {u'result': u'hello'}
+
+
+def test_authorization_3():
+    response = requests.get(url + '/__engine/1/functions/hello', headers={
+        'x-avoscloud-application-id': 'foo',
+        'x-avoscloud-application-key': 'bar',
+    })
+    assert response.status_code == 401
+
+
+def test_register_cloud_func():
+    @engine.define
+    def ping(**params):
+        assert params == {"foo": ["bar", "baz"]}
+        return 'pong'
+
+    response = requests.post(url + '/__engine/1/functions/ping', headers={
+        'x-avoscloud-application-id': TEST_APP_ID,
+        'x-avoscloud-application-key': TEST_APP_KEY,
+    }, json={'foo': ['bar', 'baz']})
+    assert response.ok
+    print response.json() == {u'result': u'pong'}
+
+
+def test_bigquery():
+    @engine.on_bigquery('end')
+    def on_bigquery_end(ok, data):
+        assert ok is False
+        assert data == {
+            "id": u"job id",
+            "status": u"OK/ERROR",
+            "message": u"当  status 为 ERROR 时的错误消息"
+        }
+
+    response = requests.post(url + '/__engine/1/functions/BigQuery/onComplete', headers={
+        'x-avoscloud-application-id': TEST_APP_ID,
+        'x-avoscloud-application-key': TEST_APP_KEY,
+    }, json={
+        "id": u"job id",
+        "status": u"OK/ERROR",
+        "message": u"当  status 为 ERROR 时的错误消息"
+    })
+    assert response.ok
+
+
+def test_client():
+    leancloud.init('pgk9e8orv8l9coak1rjht1avt2f4o9kptb0au0by5vbk9upb', 'hi4jsm62kok2qz2w2qphzryo564rzsrucl2czb0hn6ogwwnd')
+    assert cloudfunc.run('add', a=1, b=2) == 3
+
+
+def test_request_sms_code():
+    leancloud.init('pgk9e8orv8l9coak1rjht1avt2f4o9kptb0au0by5vbk9upb', master_key='azkuvukzlq3t38abdrgrwqqdcx9me6178ctulhd14wynfq1n')
+    cloudfunc.request_sms_code('13111111111')
---
+++ tests/test_2_7/test_file.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+
+from StringIO import StringIO
+from nose.tools import with_setup
+
+import requests
+
+import leancloud
+from leancloud import File
+from leancloud import ACL
+
+__author__ = 'asaka'
+
+
+def setup_func():
+    leancloud.init(
+        '6mnpbdqufkybpfwev1jww7ynrqtzv38vadgzlx37rinn9fwk',
+        'ur7kqm0qyukk2lgzzi5iqd3pym53dfdj8h3t2fksbkbdptt8',
+    )
+
+
+def test_basic():
+    s = StringIO('blah blah blah')
+    f = File('Blah', s) 
+    assert f.name == 'Blah'
+    assert f._metadata['size'] == 14
+    assert f._type == 'text/plain'
+
+
+def test_create_with_url():
+    f = File.create_with_url('xxx', 'http://www.lenna.org/full/len_std.jpg')
+    assert f.url == 'http://www.lenna.org/full/len_std.jpg'
+
+
+def test_create_without_data():
+    f = File.create_without_data(123)
+    assert f.id == 123
+
+
+def test_acl():
+    acl = ACL()
+    f = File('Blah', buffer('xxx'))
+    f.set_acl(acl)
+    assert f.get_acl() == acl
+
+
+@with_setup(setup_func)
+def test_save():
+    f = File('Blah', buffer('xxx'))
+    f.save()
+    assert f.id
+
+
+# @with_setup(setup_func)
+# def test_save_external():
+#     f = File.create_with_url('lenna.jpg', 'http://www.lenna.org/full/len_std.jpg')
+#     f.save()
+#     assert f.id
+
+
+@with_setup(setup_func)
+def test_thumbnail():
+    r = requests.get('http://www.lenna.org/full/len_std.jpg')
+    print(r)
+    b = buffer(r.content)
+    f = File('Lenna.jpg', b)
+    f.save()
+    assert f.id
+
+    url = f.get_thumbnail_url(100, 100)
+    assert url.endswith('?imageView/2/w/100/h/100/q/100/format/png')
