0f4a2902740b4e4e83d0b05d285e2c9bd93dd8aa
asaka <aisk1988@gmail.com>
2015-07-15 17:01:41 +0800

    update changelog and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='leancloud-sdk',
-    version='1.2.0',
+    version='1.2.1',
     description='LeanCloud Python SDK',
 
     url='https://leancloud.cn/',
