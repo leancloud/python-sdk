72f57dd99e11ba387cf8ac51e71cbffd15e37ae9
asaka <aisk1988@gmail.com>
2015-07-15 11:50:19 +0800

    add CORS middleware

---
+++ leancloud/engine/cors.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+
+class CORSMiddleware(object):
+    ALLOW_ORIGIN = "*"
+    ALLOW_HEADERS = ', '.join([
+        'Content-Type',
+        'X-AVOSCloud-Application-Id',
+        'X-AVOSCloud-Application-Key',
+        'X-AVOSCloud-Application-Production',
+        'X-AVOSCloud-Client-Version',
+        'X-AVOSCloud-Request-sign',
+        'X-AVOSCloud-Session-Token',
+        'X-AVOSCloud-Super-Key',
+        'X-Requested-With',
+        'X-Uluru-Application-Id,'
+        'X-Uluru-Application-Key',
+        'X-Uluru-Application-Production',
+        'X-Uluru-Client-Version',
+        'X-Uluru-Session-Token',
+    ])
+    ALLOW_METHODS = ', '.join(['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS'])
+    MAX_AGE = '86400'
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if environ['REQUEST_METHOD'] == 'OPTIONS':
+            start_response('200 OK', [
+                ('Access-Control-Allow-Origin', self.ALLOW_ORIGIN),
+                ('Access-Control-Allow-Headers', self.ALLOW_HEADERS),
+                ('Access-Control-Allow-Methods', self.ALLOW_METHODS),
+                ('Access-Control-Max-Age', self.MAX_AGE)
+            ])
+            return ['']
+        else:
+            return self.app(environ, start_response)
