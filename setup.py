73a40de50df691cceeab095dbf80dccc8b3a4fd1
asaka <aisk1988@gmail.com>
2015-09-02 14:19:42 +0800

    release 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='leancloud-sdk',
-    version='1.3.0',
+    version='1.3.1',
     description='LeanCloud Python SDK',
 
     url='https://leancloud.cn/',
