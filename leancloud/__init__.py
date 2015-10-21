f8d1de9c1c16d2c2a15ca2650a92e79e55f02270
Ān Lóng <aisk1988@gmail.com>
2015-10-21 13:30:52 +0800

    release 1.3.6

--- leancloud/__init__.py
+++ leancloud/__init__.py
@@ -31,7 +31,7 @@ from .engine.https_redirect_middleware import HttpsRedirectMiddleware
 
 
 __author__ = 'asaka <lan@leancloud.rocks>'
-__version__ = '1.3.5'
+__version__ = '1.3.6'
 
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='leancloud-sdk',
-    version='1.3.5',
+    version='1.3.6',
     description='LeanCloud Python SDK',
 
     url='https://leancloud.cn/',
