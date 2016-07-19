835afe068f77b015429341a5654b141cf96da734
DionysusG <yuan.gan@fandm.edu>
2016-07-19 17:02:57 +0800

    fixes, add verifier, and abastract validate

--- leancloud/models/field.py
+++ leancloud/models/field.py
@@ -1,5 +1,11 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import abc 
 from datetime import datetime
+import six
 
 import leancloud
 from leancloud import File as AVFile
@@ -11,7 +17,7 @@ from leancloud import acl
 
 
 class Field(with_metaclass(abc.ABCMeta)):
-    def __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def __init__(self, default=None, nullable=True, verifier=None):
         self.default = default
         self.nullable = nullable
         self.verifier = verifier
@@ -20,106 +26,103 @@ class Field(with_metaclass(abc.ABCMeta)):
     def validate(self, value):
         return
 
+    def verifier_helper(self, value):
+        if not self.verifier:
+            pass
+        else:
+            if not callable(self.verifier):
+                raise ValueError("field verifier should be a function")
+            self.verifier(value)
+
     def is_valid_empty_field(self, value):
         if self.nullable and value == None:
             return True
         return False
 
+    def validate_helper(self, value, type_list):
+        if not self.is_valid_empty_field(value):
+            if not isinstance(value, type_list):
+                raise ValueError('This field requires {0}, but {1} is {2}'.format(type_list, value, type(value)))
+            self.verifier_helper(value)
+
 
 class Number(Field):
-    def __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def __init__(self, default=None, nullable=True, verifier=None):
         super(Number, self).__init__(default, nullable, verifier)
+
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, (float, int)):
-                raise ValueError('NumField requires a value of float or int')
+        if six.PY2:
+            self.validate_helper(value, (float, int, long))
+        else:
+            self.validate_helper(value, (float, int))
 
 
 class Array(Field):
-    def __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def __init__(self, default=None, nullable=True, verifier=None):
         super(Array, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, list):
-                raise ValueError('Array Field requires the value of list type')
+        self.validate_helper(value, list)
 
 
 class Date(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(Date, self).__init__(default, nullable, verifier)
     
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, datetime):
-                raise ValueError('Datetime field requires the value of datetime.datetime type, while {0} is of {1} type'.format(value, type(value)))
+        self.validate_helper(value, datetime)
 
 
 class ACL(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(ACL, self).__init__(default, nullable, verifier)
     
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, acl.ACL):
-                raise ValueError('ACL Field requires the value of Leancloud.acl type')
+        self.validate_helper(value, acl.ACL)
 
 
 class String(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(String, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, str):
-                raise ValueError('String Field requires the value of str type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, str)
 
 
 class Boolean(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(Boolean, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, bool):
-                raise ValueError('Boolean Field requires the value of bool type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, bool)
 
 class File(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(File, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, AVFile):
-                raise ValueError('File field requires the value of Leancloud.File type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, AVFile)
 
 
 class Pointer(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
-        Object, leancloud.models.model.Model()
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(Pointer, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, (Object, leancloud.models.model.Model)):
-                raise ValueError('Pointer field requires the value of Leancloud.Object or Leancloud.Model type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, (Object, leancloud.models.model.Model))
 
 
 class User(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(User, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, (user.User, leancloud.models.model.UserModel)):
-                raise ValueError('User field requires the value of Leancloud.User type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, (user.User, leancloud.models.model.UserModel))
 
 
 class Relation(Field):
-    def  __init__(self, default=None, nullable=True, verifier=NotImplemented):
+    def  __init__(self, default=None, nullable=True, verifier=None):
         super(Relation, self).__init__(default, nullable, verifier)
 
     def validate(self, value):
-        if not self.is_valid_empty_field(value):
-            if not isinstance(value, relation.Relation):
-                raise ValueError('Relation field requires the value of Leancloud.Relation type, but {0} has a type of {1}'.format(value, type(value)))
+        self.validate_helper(value, relation.Relation)
