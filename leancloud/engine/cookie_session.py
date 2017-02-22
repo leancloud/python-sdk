6d4bb838ef9ef16bd99f7ddbf2cd31ed2ee1b4e6
Ang Long <aisk@users.noreply.github.com>
2017-02-22 13:52:08 +0800

    fix: typo in cookie session middleware (#296)

--- leancloud/engine/cookie_session.py
+++ leancloud/engine/cookie_session.py
@@ -26,27 +26,27 @@ class CookieSessionMiddleware(object):
     :type secret: str
     :param name: 在 cookie 中保存的 session token 的 key 的名称，默认为 "leancloud:session"
     :type name: str
-    :param exluded_paths: 指定哪些 URL path 不处理 session token，比如在处理静态文件的 URL path 上不进行处理，防止无谓的性能浪费
-    :type exluded_paths: list
+    :param excluded_paths: 指定哪些 URL path 不处理 session token，比如在处理静态文件的 URL path 上不进行处理，防止无谓的性能浪费
+    :type excluded_paths: list
     :param fetch_user: 处理请求时是否要从存储服务获取用户数据，如果为 false 的话，leancloud.User.get_current() 获取到的用户数据上除了 session_token 之外没有任何其他数据，需要自己调用 fetch() 来获取。为 true 的话，会自动在用户对象上调用 fetch()，这样将会产生一次数据存储的 API 调用。默认为 false
     :type fetch_user: bool
     """
-    def __init__(self, app, secret, name='leancloud:session', exluded_paths=None, fetch_user=False):
+    def __init__(self, app, secret, name='leancloud:session', excluded_paths=None, fetch_user=False):
         if not secret:
             raise RuntimeError('secret is required')
         self.fetch_user = fetch_user
         self.secret = secret
         self.app = app
         self.name = name
-        self.exluded_paths = [
+        self.excluded_paths = [
             '/__engine/',
             '/1/functions/',
             '/1.1/functions/',
             '/1/call/',
             '/1.1/call/',
         ]
-        if exluded_paths:
-            self.exluded_paths += exluded_paths
+        if excluded_paths:
+            self.excluded_paths += excluded_paths
 
     def __call__(self, environ, start_response):
         self.pre_process(environ)
@@ -58,7 +58,7 @@ class CookieSessionMiddleware(object):
 
     def pre_process(self, environ):
         request = Request(environ)
-        for prefix in self.exluded_paths:
+        for prefix in self.excluded_paths:
             if request.path.startswith(prefix):
                 return
 
