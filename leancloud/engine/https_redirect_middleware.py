c28e8020bd55b9d16fdc61a9b02429303346b6dd
Yinbin Ma <me@yinbin.ma>
2017-04-20 22:57:57 +0800

    fix: HttpsRedirectMiddleware bug in issues #303 (#307)
    * fix HttpsRedirectMiddleware bug in issues #303
    
    * fix HttpsRedirectMiddleware bug
    
    * fix HttpsRedirectMiddleware by using to_naive

--- leancloud/engine/https_redirect_middleware.py
+++ leancloud/engine/https_redirect_middleware.py
@@ -4,7 +4,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from .._compat import to_native
+
 import os
+
 
 from werkzeug.wrappers import Request
 from werkzeug.utils import redirect
@@ -13,7 +16,6 @@ __author__ = 'asaka <lan@leancloud.rocks>'
 
 
 is_prod = True if os.environ.get('LEANCLOUD_APP_ENV') == 'production' else False
-
 
 class HttpsRedirectMiddleware(object):
     def __init__(self, wsgi_app):
@@ -24,7 +26,8 @@ class HttpsRedirectMiddleware(object):
         if is_prod and request.headers.get('X-Forwarded-Proto') != 'https':
             url = 'https://{0}{1}'.format(request.host, request.path)
             if request.query_string:
-                url += '?{0}'.format(request.query_string)
+                url += '?{0}'.format(to_native(request.query_string))
+
             return redirect(url)(environ, start_response)
 
         return self.origin_app(environ, start_response)
