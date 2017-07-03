4a0daf4585ded9b53bc9d44482484234eddcdf15
Ang Long <aisk1988@gmail.com>
2017-07-03 15:26:59 +0800

    fix: add hook key header to cors headers

--- leancloud/engine/cors.py
+++ leancloud/engine/cors.py
@@ -18,11 +18,12 @@ class CORSMiddleware(object):
         'X-Uluru-Application-Production',
         'X-Uluru-Client-Version',
         'X-Uluru-Session-Token',
+        'X-LC-Hook-Key',
         'X-LC-Id',
         'X-LC-Key',
+        'X-LC-Prod',
         'X-LC-Session',
         'X-LC-Sign',
-        'X-LC-Prod',
         'X-LC-UA',
     ])
     ALLOW_METHODS = ', '.join(['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS'])
