369934220868bc34da05e0f5d4393d8bc954a06d
An Long <aisk1988@gmail.com>
2017-09-06 15:18:55 +0800

    :sparkles: support more query options in leancloud.Message.find

--- leancloud/message.py
+++ leancloud/message.py
@@ -9,11 +9,15 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from datetime import datetime
 from typing import Any
 from typing import Dict
 from typing import Generator
 from typing import List
 from typing import Optional
+from typing import Union
+
+import six
 
 from leancloud import client
 
@@ -40,28 +44,51 @@ class Message(object):
             yield msg
 
     @classmethod
-    def find_by_conversation(cls, conversation_id, limit=None, reversed=None):  # type: (str, Optional[int], Optional[bool]) -> List[Message]
+    def find_by_conversation(cls, conversation_id, client=None, limit=None, reversed=None, after_time=None, after_message_id=None):
+        # type: (str, Optional[str], Optional[int], Optional[bool], Optional[Union[datetime, float]], Optional[str]) -> List[Message]
         query_params = {}  # type: Dict[str, Any]
         query_params['convid'] = conversation_id
         if limit is not None:
             query_params['limit'] = limit
         if reversed is not None:
             query_params['reversed'] = reversed
+        if client is not None:
+            query_params['peerid'] = client
+        if isinstance(after_time, datetime):
+            query_params['max_ts'] = after_time.timestamp() * 1000
+        elif isinstance(after_time, six.integer_types) or isinstance(after_time, float):
+            query_params['max_ts'] = after_time * 1000
+        if after_message_id is not None:
+            query_params['msgid'] = after_message_id
         return list(cls._find(query_params))
 
     @classmethod
-    def find_by_client(cls, from_client, limit=None):  # type: (str, Optional[int]) -> List[Message]
+    def find_by_client(cls, from_client, limit=None, after_time=None, after_message_id=None):
+        # type: (str, Optional[int], Optional[Union[datetime, float]], Optional[str]) -> List[Message]
         query_params = {}  # type: Dict[str, Any]
         query_params['from'] = from_client
         if limit is not None:
             query_params['limit'] = limit
+        if isinstance(after_time, datetime):
+            query_params['max_ts'] = after_time.timestamp() * 1000
+        elif isinstance(after_time, six.integer_types) or isinstance(after_time, float):
+            query_params['max_ts'] = after_time * 1000
+        if after_message_id is not None:
+            query_params['msgid'] = after_message_id
         return list(cls._find(query_params))
 
     @classmethod
-    def find_all(cls, limit=None):  # type: (Optional[int]) -> List[Message]
+    def find_all(cls, limit=None, after_time=None, after_message_id=None):
+        # type: (Optional[int], Optional[Union[datetime, float]], Optional[str]) -> List[Message]
         query_params = {}  # type: Dict[str, Any]
         if limit is not None:
             query_params['limit'] = limit
+        if isinstance(after_time, datetime):
+            query_params['max_ts'] = after_time.timestamp() * 1000
+        elif isinstance(after_time, six.integer_types) or isinstance(after_time, float):
+            query_params['max_ts'] = after_time * 1000
+        if after_message_id is not None:
+            query_params['msgid'] = after_message_id
         return list(cls._find(query_params))
 
     def _update_data(self, server_data):  # type: (dict) -> None
