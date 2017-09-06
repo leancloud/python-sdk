c2691ad498009ad98cc80c1323e2192292fbe0d9
An Long <aisk1988@gmail.com>
2017-09-06 15:46:17 +0800

    :white_check_mark: add more message tests

--- tests/test_message.py
+++ tests/test_message.py
@@ -20,7 +20,7 @@ def setup():
     leancloud.init(os.environ['APP_ID'], master_key=os.environ['MASTER_KEY'])
 
 
-def test_message_find():
+def test_message_find_by_conversation():
     conv = Conversation(name='test')
     conv.save()
     conv.send('foo', 'what the hell')
@@ -36,4 +36,24 @@ def test_message_find():
     assert_equal(msg.is_room, False)
     assert_true(msg.message_id)
     assert_equal(msg.to, conv.id)
+    conv.destroy()
+
+
+def test_message_find_by_client():
+    conv = Conversation(name='test')
+    conv.save()
+    conv.send('foo', 'what the hell')
+    time.sleep(1)  # wait for server sync
+    msgs = Message.find_by_client("foo", limit=123)
+    assert len(msgs) > 0
+    conv.destroy()
+
+
+def test_message_find_all():
+    conv = Conversation(name='test')
+    conv.save()
+    conv.send('foo', 'what the hell')
+    time.sleep(1)  # wait for server sync
+    msgs = Message.find_all(limit=1000)
+    assert len(msgs) > 0
     conv.destroy()
