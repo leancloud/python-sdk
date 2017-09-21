f3e0272f8d03a72151d69d43116edeaef86b7026
An Long <aisk1988@gmail.com>
2017-09-21 12:17:04 +0800

    :wrench: support 3.6

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='Leancloud SDK',
     packages=find_packages(exclude=['docs', 'tests*']),
