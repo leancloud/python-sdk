c461fcce9ec7d41b473b28217a707347f6561af8
An Long <aisk1988@gmail.com>
2017-09-21 12:02:17 +0800

    :bookmark: bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if sys.version_info < (2, 7, 9):
 
 setup(
     name='leancloud',
-    version='2.0.0',
+    version='2.1.0',
     description='LeanCloud Python SDK',
     url='https://leancloud.cn/',
     author='asaka',
