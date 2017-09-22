1add70f1f404e7ee5f774016d4ca2d6d328b959a
An Long <aisk1988@gmail.com>
2017-09-22 14:40:46 +0800

    :bug: fix engine unicode errror

--- leancloud/engine/utils.py
+++ leancloud/engine/utils.py
@@ -7,9 +7,24 @@ from __future__ import unicode_literals
 
 import hashlib
 
+import six
+
+
 __author__ = 'asaka <lan@leancloud.rocks>'
 
 
 def sign_by_key(timestamp, key):
     s = '{0}{1}'.format(timestamp, key)
     return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+
+if six.PY2:
+    def to_native(s):
+        if isinstance(s, unicode):
+            return s.encode('utf-8')
+        return s
+else:
+    def to_native(s):
+        if isinstance(s, bytes):
+            return s.decode('utf-8')
+        return s
