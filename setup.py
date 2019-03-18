bb4e05344dc0f40e48c3a73913f5c9804bd48d03
Siyuan Miao <i@xswan.net>
2019-03-18 12:04:39 +0800

    :bookmark: release 2.1.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.version_info < (2, 7, 9):
 
 setup(
     name='leancloud',
-    version='2.1.9',
+    version='2.1.10',
     description='LeanCloud Python SDK',
     url='https://leancloud.cn/',
     author='asaka',
