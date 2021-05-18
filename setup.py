f5e1a797db61890c8527db91bbe275ff865e6cf3
Jang Rush <weakish@gmail.com>
2021-05-19 00:29:35 +0800

    docs: release 2.9.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='leancloud',
-    version='2.9.2',
+    version='2.9.3',
     description='LeanCloud Python SDK',
     url='https://leancloud.cn/',
     author='asaka',
