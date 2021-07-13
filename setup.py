893c19ad79f593a29594171f9af23d108cf40ee2
Jang Rush <weakish@gmail.com>
2021-07-13 11:50:09 +0800

    docs: bump version to 2.9.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='leancloud',
-    version='2.9.3',
+    version='2.9.4',
     description='LeanCloud Python SDK',
     url='https://leancloud.cn/',
     author='asaka',
